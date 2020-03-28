31aa33cc0b57395ede6b1cc58c589bc290a3003d
HWilliamGo <williamintoandroid@outlook.com>
2020-03-29 00:03:21 +0800

    完成

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup
 import java.io.File
 import java.io.FileReader
 import java.io.RandomAccessFile
+import java.lang.RuntimeException
 
 /**
  * date: 2019/12/11
@@ -12,13 +13,12 @@ import java.io.RandomAccessFile
  */
 
 fun main() {
-
+    val allEssay = takeTimeFromJianShuByJSoup()
+    addHeadForMD(allEssay)
 }
 
-fun takeTimeFromJianShuByJSoup() {
-    val url = "https://www.jianshu.com/nb"
-
-//    val categoryId = "";
+fun takeTimeFromJianShuByJSoup(): AllEssay {
+    //https://www.jianshu.com/nb/16737585?order_by=added_at&page=2
 
     val categoryIdPairList = getCategoryAndId()
 
@@ -31,27 +31,41 @@ fun takeTimeFromJianShuByJSoup() {
 
         //page 从1开始计数,遍历当前category下的所有的文章
         var currentPage = 1
-        var tmpList = parseByJsoup(categoryId, currentPage)
+        var tmpList = parseByJsoup(categoryId, currentPage, categoryName)
         while (tmpList.isNotEmpty()) {
             currentPage++
             tmpList.forEach { essay ->
                 allEssay.put(categoryName, essay)
             }
-            tmpList = parseByJsoup(categoryId, currentPage)
+            tmpList = parseByJsoup(categoryId, currentPage, categoryName)
         }
     }
-
-
-    //https://www.jianshu.com/nb/16737585?order_by=added_at&page=2
+    return allEssay
 }
 
-fun parseByJsoup(id: String, page: Int): List<Essay> {
+fun parseByJsoup(id: String, page: Int, categoryName: String): List<Essay> {
+    println("======类别：$categoryName")
     val result: MutableList<Essay> = ArrayList()
 
     val url = "https://www.jianshu.com/nb"
     val finalUrl = "$url/$id?order_by=added_at&page=$page"
     val document = Jsoup.connect(finalUrl).get()
+    document.getElementsByTag("li").forEach { li ->
+        val div = li.getElementsByTag("div")
+        div.forEach { eachDiv ->
+            if (eachDiv.attr("class").trim() == "content") {
+                val title = eachDiv.getElementsByTag("a").first().text()
+                var time = eachDiv
+                    .getElementsByTag("div").first()
+                    .getElementsByTag("span").filter {
+                        it.attr("class").trim() == "time"
+                    }.first().attr("data-shared-at")
+                time = timeFormatConvert(time)
 
+                println("标题：$title 创建日期：$time")
+            }
+        }
+    }
     return result
 }
 
@@ -64,18 +78,21 @@ fun getCategoryAndId(): List<CategoryIdPair> {
     val list: MutableList<CategoryIdPair> = ArrayList()
 
     for (i in 0 until arrays.length()) {
-        val id = arrays.getJSONObject(i).getString("id")
+        val id = arrays.getJSONObject(i).getInt("id").toString()
         val name = arrays.getJSONObject(i).getString("name")
         list.add(CategoryIdPair(id, name))
     }
     return list
 }
 
-fun timeFormatConvert(input: String): String {
-    return ""
+fun timeFormatConvert(i: String): String {
+    val input = i.replace("-", "/")
+    val date = input.split("T")[0]
+    val time = input.split("T")[1].split("+").first()
+    return "$date $time"
 }
 
-fun addHeadForMD(){
+fun addHeadForMD(allEssay: AllEssay) {
     val essayParentDir = "/Users/HWilliam/IdeaProjects/test/src/main/kotlin/_posts"
     val essayParentDirFile = File(essayParentDir)
 
@@ -88,8 +105,8 @@ fun addHeadForMD(){
             val fileNameWithoutMD = realFileNameOfMD.substringBefore(".")
             val tmpFileNameOfFinalMDFile = realFileNameOfMD + "tmp"
 
-
-            val headerToAppend = "---\ntitle: ${fileNameWithoutMD}\n---\n\n"
+            val date = allEssay.findTime(it.name, fileNameWithoutMD)
+            val headerToAppend = "---\ntitle: ${fileNameWithoutMD}\ndate: $date\n---\n\n"
 
             val resultFile = File(it, tmpFileNameOfFinalMDFile)
 
@@ -126,6 +143,15 @@ class AllEssay() {
             map.put(category, ArrayList())
         }
         map.get(category)?.add(essay)
+    }
+
+    fun findTime(category: String, essayName: String): String {
+        map.get(category)?.forEach {
+            if (it.title == essayName) {
+                return it.time
+            }
+        }
+        throw RuntimeException("没有找到时间 $category, $essayName")
     }
 }
 
